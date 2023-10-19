969cd12c917cea146928a281160bdc7ad4ecb9a6
thejjwang <jonathanjacobwang@gmail.com>
2023-10-19 15:27:21 -0700

    layout and structure

---
+++ bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+import {close, logo, menu} from '../assets';
+import {navLinks} from '../constants';
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center">
+
+    </nav>
+  )
+}
+
+export default Navbar
